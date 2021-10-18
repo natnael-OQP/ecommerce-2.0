7db4991f442d97dd74a8439011fb693303496a18
papa-react <makiya112545@gmail.com>
2021-10-17 20:24:04 -1200

    add matrial-ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import GlobalStyle from './GlobalStyled'
 import Shop_Data from './Data/Shop.Data'
+import PopularCategories from './Data/Icons'
+import { IconButton } from '@mui/material'
 
 const App = () => {
     return (
@@ -9,6 +11,13 @@ const App = () => {
             {
                 Shop_Data.map(({id,title,path,items}) => (
                     <h1>{title}</h1>
+                ))
+            }
+            {
+                PopularCategories.map((title,Icons) => (
+                    Icons.map((icon) => (
+                        <IconButton>{icon}</IconButton>
+                    ))
                 ))
             }
             <img src="https://dynamic.brandcrowd.com/preview/logodraft/348737be-e49b-4d53-ae50-500091b33e38/image/large.png" alt="logo" />
---
+++ src/Data/Icons.js
@@ -0,0 +1,28 @@
+import ComputerIcon from '@mui/icons-material/Computer';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import TwoWheelerIcon from '@mui/icons-material/TwoWheeler';
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
+import HeadsetIcon from '@mui/icons-material/Headset';
+import WatchIcon from '@mui/icons-material/Watch';
+import CakeIcon from '@mui/icons-material/Cake';
+
+
+const PopularCategories = [
+    {
+        title:'PopularCategories',
+        Icons: [{
+            computer:ComputerIcon,
+            phone: PhoneAndroidIcon,
+            game: SportsEsportsIcon,
+            bice: TwoWheelerIcon,
+            camera: PhotoCameraIcon,
+            Headset:HeadsetIcon,
+            watch:WatchIcon,
+            cake:CakeIcon,
+        }]
+
+    }
+]
+
+export default PopularCategories;
