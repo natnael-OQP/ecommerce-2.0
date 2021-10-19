fa44d6d7524802fbce3af3d9f6fe7d79e898ae63
papa-react <makiya112545@gmail.com>
2021-10-18 23:53:44 -1200

    style Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
+// -->
 import Header from '../component/Header/Header'
+import Hero from '../component/Hero/Hero'
+
+export const HomeWrapper = styled.div`
+    width: 100%;
+    height:100Vh;
+`;
+
+
 const HomePage = () => {
     return (
-        <>
-            <Header/>
-        </>
+        <HomeWrapper>
+            <Header />
+            <Hero/>
+        </HomeWrapper>
     )
 }
 export default HomePage
