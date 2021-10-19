612450fcf5c2addda7487cd93fc98ac99c6b78e3
papa-react <makiya112545@gmail.com>
2021-10-18 23:41:03 -1200

    header component created

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,15 +1,41 @@
+import { ArrowDownward, FavoriteOutlined, Notifications, Search, ShoppingCart,  } from '@mui/icons-material'
+import { Avatar, IconButton } from '@mui/material'
 import React from 'react'
 import {
-    HeaderContainer, Logo
+    AddCartCounter,
+    CartWrapper,
+    CategoryButton,
+    HeaderContainer, HederIconsContainer, Input, Logo, SearchContainer, SearchIconContainer, SearchIcons
 } from '.'
 import logo from  '../../logo-white.png'
 const Header = () => {
     return (
         <HeaderContainer>
-            <div>
-                <Logo src={logo} alt="logo" />
-                <
-            </div>
+            <Logo src={logo} alt="logo" />
+            {/* Search-Container--> */}
+            <SearchContainer>
+                <Input type="search" placeholder="Search..." />
+                <CategoryButton>All Category <ArrowDownward fontSize="small" /> </CategoryButton>
+                <SearchIconContainer>
+                    <Search />
+                </SearchIconContainer>
+            </SearchContainer>
+            {/* Icons-Container--> */}
+            <HederIconsContainer>
+                <IconButton>
+                    <FavoriteOutlined/>
+                </IconButton>
+                <CartWrapper>
+                    <ShoppingCart/>
+                    <AddCartCounter>1</AddCartCounter>
+                </CartWrapper>
+                <IconButton>
+                    <Notifications/>
+                </IconButton>
+                <IconButton>
+                    <Avatar />
+                </IconButton>
+            </HederIconsContainer>
         </HeaderContainer>
     )
 }
--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -6,11 +6,13 @@ export const HeaderContainer = styled.div`
     color:#ECEBEF;
     display: flex;
     align-items: center;
+    justify-content: space-between;
+    padding:0 15px;
 `;
 
 export const Logo = styled.img`
     width:150px;
-    height:40px;
+    height:30px;
     object-fit: contain;
     background-color: transparent;
 `;
@@ -19,15 +21,96 @@ export const SearchContainer = styled.div`
     flex: 1;
     display: flex;
     align-items: center;
-    height:30px;
-    width:300px;
-    background-color: #060600;
+    height:33px;
+    max-width:450px;
+    border:1px solid #303B46;
+    border-radius: 5px;
 `;
 
 export const Input = styled.input`
-    
-`;
+    height:100%;
+    width:100%;
+    padding:2px 5px;
+    font-weight: 400;
+    border-top-left-radius:5px;
+    border-bottom-left-radius:5px;
+    background-color: #212D3C;
+    `;
 
 export const CategoryButton = styled.button`
-    
+    background-color: #212D3C;
+    width:150px;
+    height:100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    white-space: nowrap;
+    border-left-style: solid;
+    border-left-color: #303B46;
+    border-left-width: .02rem;
+    font-weight:400;
+    letter-spacing: .03rem;
+    cursor: pointer;
+`;
+
+export const SearchIconContainer = styled.button`
+    width:50px;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    padding:0 .4rem;
+    height: 100%;
+    background-color: #FF9901 !important;
+    cursor: pointer;
+    border-top-right-radius:5px;
+    border-bottom-right-radius:5px;
+    .MuiSvgIcon-root{
+        color:#ECEBEF;
+        height: 100%;
+        font-size: 1.3rem;
+    }
+`;
+
+export const HederIconsContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex:.2;
+    .MuiIconButton-root{
+    }
+    .MuiSvgIcon-root{
+
+    }
+`;
+
+export const CartWrapper = styled.div`
+    position: relative;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    width:30px;
+    height:30px;
+    border-radius: 5px;
+    background-color: #FF9901 ;
+    cursor: pointer;
+    padding:5px;
+    .MuiSvgIcon-root{
+        font-size: 1.3rem;
+    }
+`;
+
+export const AddCartCounter = styled.small`
+    background-color: #ECEBEF ;
+    height:14px;
+    width:14px;
+    border-radius: 50%;
+    color:#060608;
+    position:absolute;
+    top:-7px;
+    right: -5px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    user-select: none;
+    font-weight:400;
 `;
