4f29471a2f9c0c78dc1aa6415b697f43b9879127
papa-react <makiya112545@gmail.com>
2021-10-19 21:02:37 -1200

    popular Icons and Data

---
+++ src/Data/Icons.data.js
@@ -0,0 +1,102 @@
+import { Computer,PhoneAndroid,SportsEsports,TwoWheeler,PhotoCamera,Headset,Watch,Cake } from "@mui/icons-material";
+
+
+const Icons_Data = [
+    {
+        id: 1,
+        path: 'womens',
+        Icon:Computer,
+    },
+    {
+        id: 2,
+        path: 'man',
+        Icon:PhoneAndroid,
+    },
+    {
+        id: 3,
+        path: 'man',
+        Icon:SportsEsports,
+    },
+    {
+        id: 4,
+        path: 'man',
+        Icon:TwoWheeler,
+    },
+    {
+        id: 5,
+        path: 'man',
+        Icon:PhotoCamera,
+    },
+    {
+        id: 6,
+        path: 'man',
+        Icon:Headset
+    },
+    {
+        id: 7,
+        path: 'man',
+        Icon:Watch,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    },
+    {
+        id: 8,
+        path: 'man',
+        Icon:Cake,
+    }
+];
+
+export default Icons_Data;
