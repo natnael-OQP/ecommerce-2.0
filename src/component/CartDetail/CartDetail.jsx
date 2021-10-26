d636a2faf601ed583ef5c4c59fc581947c1595c6
papa-react <makiya112545@gmail.com>
2021-10-25 21:33:22 -1200

    to fix error use in this way

--- src/component/CartDetail/CartDetail.jsx
+++ src/component/CartDetail/CartDetail.jsx
@@ -27,52 +27,52 @@ import { useHistory } from 'react-router';
 const CartDetail = () => {
     const dispatch = useDispatch();
     const cart = useSelector(selectCart);
-    const { id, imageUrl, name, brand, rate, reviews, price, oldPrice, detail, } = cart;
     const history = useHistory();
-    
     const addToBasketClick = () => {
         dispatch(addToBasket({
-            id,
-            imageUrl,
-            name,
-            brand,
-            rate,
-            reviews,
-            price,
-            oldPrice,
-            detail,
+            id:cart.id,
+            imageUrl:cart.imageUrl,
+            name:cart.name,
+            brand:cart.brand,
+            rate:cart.rate,
+            reviews:cart.reviews,
+            price:cart.price,
+            oldPrice:cart.oldPrice,
+            detail:cart.detail,
         }))
         history.push('/basket')
     }
     return (
         <CartWrapper>
-        <ArrowBack fontSize="medium" onClick={()=>history.push('/')} />
-            <Wrapper>
-                <ImageContainer>
-                    <Image src={imageUrl} alt={name} />
-                </ImageContainer>
-                <InfoContainer>
-                    <Name>{name}</Name>
-                    <Brand>{brand}</Brand>
-                    <RateAndReview>
-                        <StarRate fontSize="small" />
-                        <StarRate fontSize="small" />
-                        <StarRate fontSize="small" />
-                        <StarRate fontSize="small" />
-                        <StarRate fontSize="small" />
-                        <Rate>{rate}</Rate>
-                        <Review>{reviews} </Review>
-                    </RateAndReview>
-                    <PriceContainer>
-                        <Price>{price}<Birr>ብር</Birr></Price>
-                        <OldPrice>{oldPrice}<small>ብር</small></OldPrice>
-                    </PriceContainer>
-                    <Detail>{detail}</Detail>
-                    <AddToCart
-                    onClick={addToBasketClick}
-                    >add to Cart </AddToCart>
-                </InfoContainer>
-            </Wrapper>
+            <ArrowBack fontSize="medium" onClick={()=>history.push('/')} />
+            {cart === null ? <h1>empty 😒 </h1> : (
+                <Wrapper>
+                    <ImageContainer>
+                        <Image src={cart.imageUrl} alt={cart.name} />
+                    </ImageContainer>
+                    <InfoContainer>
+                        <Name>{cart.name}</Name>
+                        <Brand>{cart.brand}</Brand>
+                        <RateAndReview>
+                            <StarRate fontSize="small" />
+                            <StarRate fontSize="small" />
+                            <StarRate fontSize="small" />
+                            <StarRate fontSize="small" />
+                            <StarRate fontSize="small" />
+                            <Rate>{cart.rate}</Rate>
+                            <Review>{cart.reviews} </Review>
+                        </RateAndReview>
+                        <PriceContainer>
+                            <Price>{cart.price}<Birr>ብር</Birr></Price>
+                            <OldPrice>{cart.oldPrice}<small>ብር</small></OldPrice>
+                        </PriceContainer>
+                        <Detail>{cart.detail}</Detail>
+                        <AddToCart
+                        onClick={addToBasketClick}
+                        >add to Cart </AddToCart>
+                    </InfoContainer>
+                </Wrapper>
+            )}
         </CartWrapper>
     )
 }
