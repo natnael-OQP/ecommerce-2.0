594d30cc10ddcf9b003f5a6b7dbabfbd5c81297b
papa-react <makiya112545@gmail.com>
2021-10-28 22:21:31 -1200

    remove counter Slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counterSlice';
 import cartReducer from '../features/cartSlice';
 import basketReducer from '../features/basketSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     cart:cartReducer,
     basket:basketReducer,
   },
