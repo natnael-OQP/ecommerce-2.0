41d64a4f4fbe464f95e5c3f3d400d41df395b824
papa-react <makiya112545@gmail.com>
2021-10-28 22:16:26 -1200

    make addtobasket and quantity counter

--- src/features/basketSlice.js
+++ src/features/basketSlice.js
@@ -4,16 +4,25 @@ import { createSlice } from '@reduxjs/toolkit';
 export const basketSlice = createSlice({
     name: 'basket',
     initialState:{
-        items:[],
+        items: [],
     },
     reducers: {
         addToBasket: (state, action) => {
+            const isExist = state.items.find((item) => item.id === action.payload.id);
+            if (isExist) {
+                isExist.quantity++;
+                isExist.price += action.payload.price;
+            } else {
                 state.items=[...state.items , action.payload]
+            }
         },
         removeFromBasket: (state, action) => {
+            const isExist = state.items.find((item) => item.id === action.payload.id);
             const index = state.items.findIndex((item) => item.id === action.payload.id);
             let newBasket = [...state.items];
-            if (index >= 0) {
+            if (index >= 0 && isExist) {
+                isExist.quantity--;
+                isExist.price -= action.payload.price;
                 newBasket.splice(index, 1);
             }
             state.items = newBasket;
@@ -24,6 +33,5 @@ export const basketSlice = createSlice({
 
 export const { addToBasket,removeFromBasket } = basketSlice.actions;
 export const selectItems = (state) => state.basket.items;
-
 
 export default basketSlice.reducer;
