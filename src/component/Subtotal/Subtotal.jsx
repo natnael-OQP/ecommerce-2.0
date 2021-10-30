da189e4f0e10581bab869ff98f333a152a0e333a
papa-react <makiya112545@gmail.com>
2021-10-29 17:27:21 -1200

    subtotal ui part

--- src/component/Subtotal/Subtotal.jsx
+++ src/component/Subtotal/Subtotal.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { SubTotalContainer } from '.'
+import { Info, Number,CheckOutBtn, SubTotalContainer, Rows, PiceValue, Info1, Info3, Info3Container, } from '.'
+import { Birr} from '../basketCart'
 
 const Subtotal = () => {
     return (
         <SubTotalContainer>
-            hello
+            <Rows>
+                <Info>Order Summary</Info>
+                <Number>1 items</Number>
+            </Rows>
+            <Rows>
+                <Info1>Subtotal :</Info1>
+                <PiceValue>2000<Birr>ብር</Birr></PiceValue>
+            </Rows>
+            <Rows>
+                <Info1>Delivery Charges :</Info1>
+                <Info3Container>
+                    <Info3>Add your Delivery</Info3>
+                    <Info3>address at checkout to</Info3>
+                    <Info3>see delivery charges</Info3>
+                </Info3Container>
+            </Rows>
+            <Rows>
+                <Info>Total</Info>
+                <PiceValue>2000<Birr>ብር</Birr></PiceValue>
+            </Rows>
+            <CheckOutBtn>Continue to Checkout</CheckOutBtn>
         </SubTotalContainer>
     )
 }
--- src/component/Subtotal/index.js
+++ src/component/Subtotal/index.js
@@ -1,8 +1,64 @@
 import styled from 'styled-components';
 
 export const SubTotalContainer = styled.div`
-    flex:.2;
+    flex:.27;
+    height:280px;
+    padding:.4rem;
+    padding-top:.8rem;
     background-color: #fff;
-    height: 100%;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+`;
+
+export const Rows = styled.div`
+    border-bottom: 1px solid whitesmoke;
+    padding:.6rem .1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const Info = styled.h5`
+    font-weight: 600;
+`;
+
+export const Info1 = styled.h5`
+    font-weight: 400;
+`;
+
+export const Info3Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+`;
+
+export const Info3 = styled.h6`
+    font-size: 9px;
+    font-weight: 400;
+    color:gray;
+`;
+
+export const Number = styled.h6`
+    font-weight: 600;
+    color:gray;
+`;
+
+export const PiceValue = styled.h4`
+    font-weight:400;
+    letter-spacing: .03rem;
+    color:#303B46;
+`;
+
+export const CheckOutBtn = styled.button`
+    cursor: pointer;
+    display:flex;
+    align-items: center;
+    justify-content:center;
+    width:100%;
+    height:35px;
+    /* margin-top:1rem; */
+    background-color: #FF9901;
+    color:#fff;
+    font-weight: 400;
+    border-radius:3px;
+    font-size: 1rem;
 `;
