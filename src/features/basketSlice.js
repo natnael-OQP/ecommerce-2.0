a76eea9ea30d07af5f6e756e94dac5ca2615efe3
papa-react <makiya112545@gmail.com>
2021-10-29 19:24:35 -1200

    when there is no data in the items list, then data and create new original price  items

--- src/features/basketSlice.js
+++ src/features/basketSlice.js
@@ -11,27 +11,47 @@ export const basketSlice = createSlice({
             const isExist = state.items.find((item) => item.id === action.payload.id);
             if (isExist) {
                 isExist.quantity++;
-                isExist.price += action.payload.price;
+                isExist.price += isExist.originalPrice;
             } else {
-                state.items=[...state.items , action.payload]
+                const data = action.payload;
+                state.items.push({
+                    id: data.id,
+                    name: data.name,
+                    imageUrl:data.imageUrl,
+                    rate:data.rate,
+                    reviews: data.review,
+                    price:data.price,
+                    quantity: data.quantity,
+                    originalPrice:data.price,
+                })
             }
         },
         removeFromBasket: (state, action) => {
             const isExist = state.items.find((item) => item.id === action.payload.id);
-            const index = state.items.findIndex((item) => item.id === action.payload.id);
+            const index = state.items.findIndex((item) => item.id === action.payload.id)
             let newBasket = [...state.items];
-            if (index >= 0 && isExist) {
+            if (isExist.quantity > 1  ) {
                 isExist.quantity--;
-                isExist.price -= action.payload.price;
+                isExist.price -= isExist.originalPrice;
+            }else{
                 newBasket.splice(index, 1);
             }
             state.items = newBasket;
-        }
+        },
+        Delate: (state, action) => {
+            const index = state.items.findIndex((item) => item.id === action.payload.id);
+            let newBasket = [...state.items];
+            if (index >= 0 ) {
+                newBasket.splice(index, 1);
+            }
+            state.items = newBasket;
+        },
     }
 });
 
 
-export const { addToBasket,removeFromBasket } = basketSlice.actions;
+export const { addToBasket,removeFromBasket,Delate } = basketSlice.actions;
 export const selectItems = (state) => state.basket.items;
-
+export const selectTotal = (state) => state.basket.items.reduce((total, item) => total + item?.price, 0);
+export const selectTotalQuantity = (state) => state.basket.items.reduce((total, item) => total + item?.quantity, 0);
 export default basketSlice.reducer;
