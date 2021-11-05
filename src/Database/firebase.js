64f4d9c296d20e3dde57f83de3dc0f68aade680f
papa-react <makiya112545@gmail.com>
2021-11-05 11:21:44 -1200

    add firebase SDK

--- src/Database/firebase.js
+++ src/Database/firebase.js
@@ -3,7 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyDlR-sSzFsPMBwQX7ovV0LnvyUERJzGSnA",
+    authDomain: "ecommerce-2-cb758.firebaseapp.com",
+    projectId: "ecommerce-2-cb758",
+    storageBucket: "ecommerce-2-cb758.appspot.com",
+    messagingSenderId: "1065008691486",
+    appId: "1:1065008691486:web:a61de2eb35733ea37a310f"
 };
 
 firebase.initializeApp(firebaseConfig);
