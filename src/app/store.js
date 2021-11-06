141bb5bb139c9d3b07cb4d6fb6d0b631419b5f4a
papa-react <makiya112545@gmail.com>
2021-11-05 12:09:42 -1200

    add userSlice to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/cartSlice';
 import basketReducer from '../features/basketSlice';
 import searchReducer from '../features/searchSlice';
+import userReducer from '../features/userSlice';
 
 export const store = configureStore({
   reducer: {
     cart:cartReducer,
     basket: basketReducer,
     search: searchReducer,
+    user: userReducer,
   },
 });
