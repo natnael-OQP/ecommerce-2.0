5be9cf621ef9bc4c152665015ff9ac8b3842c91a
papa-react <makiya112545@gmail.com>
2021-11-05 12:09:19 -1200

    when user logedin before stay update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { auth } from './Database/firebase'
+import { signIn, signOut } from './features/userSlice'
 import GlobalStyle from './GlobalStyled'
 import HomePage from './pages/HomePage'
 
 const App = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                dispatch(signIn({
+                    email: user.email,
+                    name: user.displayName,
+                    photo: user.photoURL,
+                }))
+            } else {
+                dispatch(signOut());
+            }
+        })
+    },[])
     return (
         <>
             <GlobalStyle />
