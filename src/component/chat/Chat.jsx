f9f79092015bb8ce54797e7661f3717a05ad28db
papa-react <makiya112545@gmail.com>
2021-11-06 13:33:40 -1200

    chat componet

---
+++ src/component/chat/Chat.jsx
@@ -0,0 +1,19 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+import {
+    ChatContainer,
+    ReviewsContent
+} from '.'
+
+const Chat = ({photo,review}) => {
+    return (
+        <ChatContainer>
+            <Avatar />
+            <ReviewsContent>
+                rich sfhudfh uwfeyg eyrgf
+            </ReviewsContent>
+        </ChatContainer>
+    )
+}
+
+export default Chat
---
+++ src/component/chat/index.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ChatContainer = styled.div`
+    margin-top:1rem;
+    display: flex;
+    align-items: center;
+    border-radius: 5px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+    background-color: #E6E9EA;
+    padding:.5rem;
+`;
+
+export const ReviewsContent = styled.h5`
+    font-weight: 600;
+    margin-left: .5rem;
+    word-wrap: break-word;
+`;
