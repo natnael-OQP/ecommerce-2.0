2e6bce0fa42d2dfb39029e923967e66e24275039
papa-react <makiya112545@gmail.com>
2021-11-06 20:39:36 -1200

    toogle the message box

--- src/component/reviews/Reviews.jsx
+++ src/component/reviews/Reviews.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import { RateReview } from '@mui/icons-material';
+import { Button } from '@mui/material';
+import React ,{useState} from 'react'
 import { useSelector } from 'react-redux';
 import {
     ReviewsContainer,
     Form,
     Message,
+    Header,
+    Body,
+    Btn,
+    H4,
 } from '.';
-import { selectCart } from '../../features/cartSlice';
-import Chat from '../chat/Chat';
-const Reviews = () => {
-    const reviewID = useSelector(selectCart);
 
+import Chat from '../chat/Chat';
+
+const Reviews = () => {
+    const [open,setOpen] = useState(false);
+    const toggle = () => {
+        setOpen(!open);
+    }
+    const [input, setInput] = useState('');
+    const sendReview = (e) => {
+        e.preventDefault();
+    }
+    
     return (
         <ReviewsContainer>
-            <Form>
-                <Message   />
-            </Form>
-        <Chat />
+            <Header>
+                <H4>Positive Reviews</H4>
+                <Button>
+                    <RateReview onClick={toggle} />
+                </Button>
+            </Header>
+            <Body>
+                <Form open={open} >
+                    <Message value={input} onChange={(e) => setInput(e.target.value) } placeholder="wright your Message..." cols="60" rows="6"  />
+                    <Btn type="submit" onClick={sendReview} >Sent</Btn>
+                </Form>
+                <Chat />
+                <Chat />
+                <Chat />
+            </Body>
         </ReviewsContainer>
 )}
 
