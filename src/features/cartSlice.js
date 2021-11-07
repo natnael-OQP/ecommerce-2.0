e8925aa47616b071c37aa837d2b9825c454f86d4
papa-react <makiya112545@gmail.com>
2021-11-06 21:37:29 -1200

    add id in state

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -4,11 +4,13 @@ import { createSlice } from '@reduxjs/toolkit';
 export const cartSlice = createSlice({
     name: 'cart',
     initialState:{
+        id:null,
         cartInfo:null,
     },
     reducers: {
         cartInfo: (state, action) => {
-        state.cartInfo =  action.payload;
+            state.cartInfo = action.payload;
+            state.id = action.payload.id;
         },
     }
 });
@@ -16,6 +18,7 @@ export const cartSlice = createSlice({
 export const { cartInfo } = cartSlice.actions;
 
 export const selectCart = (state) => state.cart.cartInfo;
+export const selectID = (state) => state.cart.id;
 
 
 export default cartSlice.reducer;
