ed883076d7afdd652366fbe39495e506cb96d6f2
papa-react <makiya112545@gmail.com>
2021-11-06 21:37:02 -1200

    sent message and pull messages from firestore

--- src/component/reviews/Reviews.jsx
+++ src/component/reviews/Reviews.jsx
@@ -1,6 +1,6 @@
 import { RateReview } from '@mui/icons-material';
 import { Button } from '@mui/material';
-import React ,{useState} from 'react'
+import React ,{useEffect, useState} from 'react'
 import { useSelector } from 'react-redux';
 import {
     ReviewsContainer,
@@ -12,16 +12,50 @@ import {
     H4,
 } from '.';
 
+import { selectCart, selectID } from '../../features/cartSlice';
 import Chat from '../chat/Chat';
+import {db} from '../../Database/firebase'
+import firebase from 'firebase/compat/app';
+import { selectUser } from '../../features/userSlice';
 
 const Reviews = () => {
     const [open,setOpen] = useState(false);
+    const [input, setInput] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ID = useSelector(selectID);
+    const user = useSelector(selectUser);
+    useEffect(() => {
+        if (ID) {
+            db.collection('reviews')
+                .doc(ID)
+                .collection('messages')
+                .orderBy('timeStamp', 'desc')
+                .onSnapshot((snapshot) => (
+                    setMessages(
+                        snapshot.docs.map((doc) => ({
+                            data: doc.data(),
+                        }))
+                    )
+                ))
+        }
+    }, [ID]);
+    // toggle
+    console.log(ID);
     const toggle = () => {
         setOpen(!open);
     }
-    const [input, setInput] = useState('');
+    // send Message
     const sendReview = (e) => {
         e.preventDefault();
+        db.collection('reviews').doc(ID).collection('messages').add({
+            message: input,
+            timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+            name: user.name,
+            photo: user.photo,
+            email: user.email,
+            id: user.id,
+        })
+        setInput("")
     }
     
     return (
@@ -37,9 +71,12 @@ const Reviews = () => {
                     <Message value={input} onChange={(e) => setInput(e.target.value) } placeholder="wright your Message..." cols="60" rows="6"  />
                     <Btn type="submit" onClick={sendReview} >Sent</Btn>
                 </Form>
-                <Chat />
-                <Chat />
-                <Chat />
+                {
+                    messages.map(({id,data}) => (
+                        <Chat key={id}  data={data} />
+                    ))
+                }
+                
             </Body>
         </ReviewsContainer>
 )}
