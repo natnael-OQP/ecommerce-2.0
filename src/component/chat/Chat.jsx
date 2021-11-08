8162d5c396ade40dabbb1df9cb00a367ad2307da
papa-react <makiya112545@gmail.com>
2021-11-07 16:45:39 -1200

    get name image and message from db and style them

--- src/component/chat/Chat.jsx
+++ src/component/chat/Chat.jsx
@@ -1,17 +1,28 @@
+import React,{forwardRef} from 'react'
 import { Avatar } from '@mui/material'
-import React from 'react'
 import {
     ChatContainer,
-    ReviewsContent
+    Header,
+    Name,
+    ReviewsContent,
+    Time
 } from '.'
+import * as timeago from 'timeago.js';
 
-const Chat = ({id,data:{message,name,photo,timeStamp,}}) => {
+
+const Chat = forwardRef(({ content: { messages, displayName, photo, timestamp } },ref) => {
     return (
-        <ChatContainer>
-            <Avatar src={photo} />
-            <ReviewsContent>{message}</ReviewsContent>
-        </ChatContainer>
+        <>
+            <ChatContainer ref={ref}>
+                <Header>
+                    <Avatar src={photo} alt={displayName} />
+                    <Name>{displayName}</Name>
+                    <Time>{timeago.format(new Date(timestamp?.toDate()))}</Time>
+                </Header>
+                <ReviewsContent>{messages}</ReviewsContent>
+            </ChatContainer>
+        </>
     )
-}
+})
 
 export default Chat
--- src/component/chat/index.js
+++ src/component/chat/index.js
@@ -3,15 +3,40 @@ import styled from 'styled-components';
 export const ChatContainer = styled.div`
     margin-top:1rem;
     display: flex;
-    align-items: center;
+    flex-direction: column;
     border-radius: 5px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
     background-color: #E6E9EA;
     padding:.5rem;
 `;
 
+export const Header = styled.div`
+    display:flex;
+    align-items: center;
+    margin-bottom: .5rem;
+    padding:0 .5rem;
+    position: relative;
+`;
+
+export const Name = styled.h5`
+    color: #355B98;
+    font-weight:600;
+    margin-left: .5rem;
+`;
+
 export const ReviewsContent = styled.h5`
     font-weight: 600;
     margin-left: .5rem;
     word-wrap: break-word;
+    padding:.2rem 1.2rem;
+    color:#32333B;
 `;
+export const Time = styled.h4`
+    position: absolute;
+    top:.2rem;
+    right:.5rem;
+    font-weight:400;
+    font-size:12px;
+    color:gray;
+`;
+
