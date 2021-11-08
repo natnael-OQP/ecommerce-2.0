70d503091d160c8e0d9dfd76b639407383583543
papa-react <makiya112545@gmail.com>
2021-11-07 17:40:22 -1200

    user not loged in yet login

--- src/component/Subtotal/Subtotal.jsx
+++ src/component/Subtotal/Subtotal.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Info, Number,CheckOutBtn, SubTotalContainer, Rows, PiceValue, Info1, Info3, Info3Container, } from '.'
+import { auth, provider } from '../../Database/firebase'
 import { selectTotal, selectTotalQuantity } from '../../features/basketSlice'
+import { selectUser, signIn } from '../../features/userSlice'
 import { Birr} from '../basketCart'
 
 const Subtotal = () => {
     const total = useSelector(selectTotal);
     const items = useSelector(selectTotalQuantity);
-
+    const user = useSelector(selectUser)
+    const dispatch = useDispatch();
+    const SignIn = () => {
+        auth.signInWithPopup(provider).then(({user}) => {
+            dispatch(signIn({
+                email: user.email,
+                name: user.displayName,
+                photo: user.photoURL,
+            }))
+        }).catch((error) => alert(error))
+    }
     return (
         <SubTotalContainer>
             <Rows>
@@ -30,7 +42,13 @@ const Subtotal = () => {
                 <Info>Total</Info>
                 <PiceValue>{total}<Birr>ብር</Birr></PiceValue>
             </Rows>
-            <CheckOutBtn role="link" >Continue to Checkout</CheckOutBtn>
+            {
+                user ? (
+                    <CheckOutBtn role="link" >Continue to Checkout</CheckOutBtn>
+                ): (
+                    <CheckOutBtn onClick={SignIn}  >Login</CheckOutBtn>
+                )
+            }
         </SubTotalContainer>
     )
 }
